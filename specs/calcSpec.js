87dfbd305918d64aa6b879c29ff943bacb9a4e3d
mariusz79 <mpawlowic@gmail.com>
2018-10-24 11:01:19 +0100

    add test and function

---
+++ specs/calcSpec.js
@@ -0,0 +1,7 @@
+describe("Calculator", function(){                  //description
+    describe("Addition tests", function(){           //additional test
+        it("should return 42", function(){          //assertion
+            expect(addition(20,22)).toBe(42);                                       //expectation
+        })
+    })                                                   
+})
