52a15ea3d6bdb59779e907bb23ab3a7bf05386e9
mariusz79 <mpawlowic@gmail.com>
2018-10-24 11:13:49 +0100

    add extra test, changing addition function

--- specs/calcSpec.js
+++ specs/calcSpec.js
@@ -1,7 +1,10 @@
 describe("Calculator", function(){                  //description
-    describe("Addition tests", function(){           //additional test
+    describe("Addition tests", function(){          //additional descriptiona nd test
         it("should return 42", function(){          //assertion
             expect(addition(20,22)).toBe(42);       //expectation, addition is name of the function defined in calc.js
+        });
+        it("should return 26", function(){          //assertion
+            expect(addition(7,19)).toBe(26);        //expectation
         })
-    })                                                   
-})
+    });                                                   
+});
