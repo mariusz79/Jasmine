96772286d5ac43822c650f4fcdd2394cff12c0d1
mariusz79 <mpawlowic@gmail.com>
2018-10-24 11:28:28 +0100

    add extra test, modify adding function

--- specs/calcSpec.js
+++ specs/calcSpec.js
@@ -5,6 +5,9 @@ describe("Calculator", function(){                  //description
         });
         it("should return 26", function(){          //assertion
             expect(addition(7,19)).toBe(26);        //expectation
-        })
+        });
+        it("should return an error if we don't supply two numbers", function(){     //assertion
+            expect(addition('Hitchhikers', 'Guide')).toBe("Error!");                //expectation
+        });
     });                                                   
 });
